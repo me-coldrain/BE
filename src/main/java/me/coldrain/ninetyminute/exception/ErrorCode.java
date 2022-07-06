0f6943380f9f267f557e630c84f08e1b3df13b7a
guluming <guluming@naver.com>
2022-07-06 23:12:07 +0900

    Feat: "추가 ErrorCode 클래스"
    로그인 및 jwt 토큰 관련 exception 메세지 설정 기능

---
+++ src/main/java/me/coldrain/ninetyminute/exception/ErrorCode.java
@@ -0,0 +1,29 @@
+package me.coldrain.ninetyminute.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    SingUpDataValidException (400, "회원가입 양식을 확인해 주세요.", HttpStatus.BAD_REQUEST),
+    UsernameOrPasswordNotFoundException (400, "아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
+    ForbiddenException(403, "해당 요청에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    UNAUTHORIZEDException (401, "로그인 후 이용가능합니다.", HttpStatus.UNAUTHORIZED),
+    ExpiredJwtException(444, "Access 토큰이 만료되었습니다. Refresh 토큰으로 Access 토큰을 재발급 해주세요.", HttpStatus.UNAUTHORIZED),
+    ReLogin(445, "모든 토큰이 만료되었습니다. 다시 로그인해주세요.", HttpStatus.UNAUTHORIZED),
+    ;
+
+    @Getter
+    private int code;
+
+    @Getter
+    private String message;
+
+    @Getter
+    private HttpStatus status;
+
+    ErrorCode(int code, String message, HttpStatus status) {
+        this.code = code;
+        this.message = message;
+        this.status = status;
+    }
+}
