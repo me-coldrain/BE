43e1032c74ffd8b1fbcfd1336db69c087745a096
guluming <guluming@naver.com>
2022-07-07 17:05:43 +0900

    Feat: "수정 Member 클래스"
    kakao사용자 식별을 위한 변수 추가
    프로필 사진 수정시 기존 사진 삭제를 위한 식별 변수 추가
    일반사용자 회원가입 데이터 저장 기능 추가
    카카오사용자 회원가입 데이터 저장 기능 추가
    모든사용자 최초 로그인 & 회원정보 수정 기능 추가
    비밀번호 저장시 단방향 암호화 기능 추가

--- src/main/java/me/coldrain/ninetyminute/entity/Member.java
+++ src/main/java/me/coldrain/ninetyminute/entity/Member.java
@@ -4,8 +4,12 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import me.coldrain.ninetyminute.dto.request.MemberEditRequest;
+import me.coldrain.ninetyminute.dto.request.MemberRegisterRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
+import java.util.Map;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,23 +29,54 @@ public class Member extends TimeStamped {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ability_id")
     private Ability ability; // 능력치
-
+    @Column(unique = true)
+    private Long kakaoId;
+    @Column(nullable = false, unique = true)
     private String username;
-
+    @Column(nullable = false)
     private String password;
-
+    @Column(nullable = false, unique = true)
     private String nickname;
-
+    @Column
+    private String profilename;
+    @Column
     private String profileUrl;
-
+    @Column(nullable = false)
     private String position;
-
+    @Column(nullable = false)
     private String contact;
-
+    @Column
     private String phone;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private MemberRoleEnum role;
+
+    public Member(MemberRegisterRequest params, MemberRoleEnum role) {
+        this.username = params.getEmail();
+        this.password = params.getPassword();
+        this.role = role;
+        this.kakaoId = null;
+    }
+
+    public Member(String password, MemberRoleEnum role, Long kakaoId) {
+        this.username = null;
+        this.password = password;
+        this.role = role;
+        this.kakaoId = kakaoId;
+    }
+
+    public void memberUpdate(Map<String, String> profileImg, MemberEditRequest params){
+        this.nickname = params.getNickname();
+        this.profilename = profileImg.get("transImgFileName");
+        this.profileUrl = profileImg.get("url");
+        this.position = params.getPosition();
+        this.contact = params.getContact();
+        this.phone = params.getPhone();
+    }
+
+    public void encryptPassword(PasswordEncoder passwordEncoder) {
+        password = passwordEncoder.encode(password);
+    }
 
 }
