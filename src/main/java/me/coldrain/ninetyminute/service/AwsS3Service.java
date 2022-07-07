c4a54cc3fa439dd75528024e0d487737fd5ba46f
guluming <guluming@naver.com>
2022-07-07 17:18:01 +0900

    Style: "수정 AwsS3Service 클래스"
    사용하지 않는 변수 제외

--- src/main/java/me/coldrain/ninetyminute/service/AwsS3Service.java
+++ src/main/java/me/coldrain/ninetyminute/service/AwsS3Service.java
@@ -47,7 +47,7 @@ public class AwsS3Service {
 
         Map<String , String> result = new HashMap<>();
         result.put("url" , String.valueOf(amazonS3.getUrl(bucket,fileName)));
-        result.put("fileName" , rawFileName);
+//        result.put("fileName" , rawFileName);
         result.put("transImgFileName", fileName);
         return result;
     }
