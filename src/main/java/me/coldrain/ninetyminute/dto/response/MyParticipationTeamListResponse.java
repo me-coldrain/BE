36061d1bd3dcc9ab7625e880b4cb999be38d9871
guluming <guluming@naver.com>
2022-07-12 15:53:08 +0900

    Feat: "추가 마이페이지 조회 API"
    - recruit, match는 어디서 가져와야 하는지 몰라서 보류
    - 경기 히스토리도 작성 되지 않아서 보류

---
+++ src/main/java/me/coldrain/ninetyminute/dto/response/MyParticipationTeamListResponse.java
@@ -0,0 +1,32 @@
+package me.coldrain.ninetyminute.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import me.coldrain.ninetyminute.entity.Time;
+import me.coldrain.ninetyminute.entity.Weekday;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class MyParticipationTeamListResponse {
+    private Long teamId;
+    private String teamName;
+    private int headCount;
+    private String mainArea;
+    private String preferredArea;
+    private List<Weekday> weekdays;
+    private List<Time> time;
+    private double winRate;
+    private boolean recruit;
+    private boolean match;
+    private int totalGameCount;
+    private int winCount;
+    private int drawCount;
+    private int loseCount;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+}
