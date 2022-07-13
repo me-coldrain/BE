85b97fba372ad422c035f71013bf2315ae50fe56
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-07-13 14:18:39 +0900

    Feat: "추가 DELETE 대결 신청 거절 API"

--- src/main/java/me/coldrain/ninetyminute/controller/MatchingController.java
+++ src/main/java/me/coldrain/ninetyminute/controller/MatchingController.java
@@ -31,4 +31,18 @@ public class MatchingController {
 
         return matchingService.approvedMatch(apply_team_id, approvedMatchRequest, userDetails.getUser());
     }
+
+    /*
+     * Author: 병민
+     * 대결 신청 거절 API
+     * 대결 신청 팀의 id을 전달 받아야 함.
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/teams/{apply_team_id}/apply")
+    public void cancelApplyMatch(
+            final @PathVariable Long apply_team_id,
+            final @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        matchingService.rejectApplyMatch(apply_team_id, userDetails.getUser().getOpenTeam().getId());
+    }
 }
