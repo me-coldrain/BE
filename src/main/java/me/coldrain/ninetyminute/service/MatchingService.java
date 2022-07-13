24f17ea41697c10f690199109972857233d00b52
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-07-13 11:20:15 +0900

    Fix: "수정 같은 팀이 수락하는 것을 방지, validation 추가"

--- src/main/java/me/coldrain/ninetyminute/service/MatchingService.java
+++ src/main/java/me/coldrain/ninetyminute/service/MatchingService.java
@@ -22,28 +22,30 @@ public class MatchingService {
 
     @Transactional
     public String approvedMatch(Long apply_team_id, ApprovedMatchRequest approvedMatchRequest, Member member) {
-        String teamName = teamRepository.findById(member.getOpenTeam().getId()).orElseThrow(
-                () -> new IllegalArgumentException("팀을 찾을 수 없습니다.")).getName();
+        if (!apply_team_id.equals(member.getOpenTeam().getId())) {
+            String teamName = teamRepository.findById(member.getOpenTeam().getId()).orElseThrow(
+                    () -> new IllegalArgumentException("팀을 찾을 수 없습니다.")).getName();
 
-        String opposingTeamName = teamRepository.findById(apply_team_id).orElseThrow(
-                () -> new IllegalArgumentException("팀을 찾을 수 없습니다.")).getName();
+            String opposingTeamName = teamRepository.findById(apply_team_id).orElseThrow(
+                    () -> new IllegalArgumentException("팀을 찾을 수 없습니다.")).getName();
 
-        Apply applyMatch = applyRepository.findByApplyTeamIdAndTeamId(apply_team_id, member.getOpenTeam().getId()).orElseThrow(
-                () -> new IllegalArgumentException("신청한 팀을 찾을 수 없습니다.")
-        );
+            Apply applyMatch = applyRepository.findByApplyTeamIdAndTeamId(apply_team_id, member.getOpenTeam().getId()).orElseThrow(
+                    () -> new IllegalArgumentException("신청한 팀을 찾을 수 없습니다.")
+            );
 
-        applyMatch.changeApproved(true);
-        applyRepository.save(applyMatch);
+            applyMatch.changeApproved(true);
+            applyRepository.save(applyMatch);
 
-        BeforeMatching beforeMatching = BeforeMatching.builder()
-                .apply(applyMatch)
-                .matchDate(approvedMatchRequest.getMatchDate())
-                .location(approvedMatchRequest.getMatchLocation())
-                .opposingTeamName(opposingTeamName)
-                .teamName(teamName)
-                .build();
+            BeforeMatching beforeMatching = BeforeMatching.builder()
+                    .apply(applyMatch)
+                    .matchDate(approvedMatchRequest.getMatchDate())
+                    .location(approvedMatchRequest.getMatchLocation())
+                    .opposingTeamName(opposingTeamName)
+                    .teamName(teamName)
+                    .build();
 
-        beforeMatchingRepository.save(beforeMatching);
-        return "완료됐습니다.";
+            beforeMatchingRepository.save(beforeMatching);
+            return "완료됐습니다.";
+        } else throw new IllegalArgumentException("자신의 팀에 대결 신청은 불가능합니다.");
     }
 }
