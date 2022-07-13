c1d89f349847eeb516b0d92b392e519719232155
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-07-14 02:58:07 +0900

    Feat: "추가 POST 대결 종료 확인 API"

--- src/main/java/me/coldrain/ninetyminute/entity/Apply.java
+++ src/main/java/me/coldrain/ninetyminute/entity/Apply.java
@@ -30,8 +30,20 @@ public class Apply extends TimeStamped {
 
     private Boolean approved;
 
+    // 대결 후 대결 종료 버튼을 누를때 변경되어야 하는 사항이 있어야 한다.
+    private Boolean endMatchStatus;
+    private Boolean opposingTeamEndMatchStatus;
+
     public void changeApproved(Boolean approved) {
         this.approved = approved;
+    }
+
+    public void changeEndMatchStatus(Boolean endMatchStatus) {
+        this.endMatchStatus = endMatchStatus;
+    }
+
+    public void changeOpposingTeamEndMatchStatus(Boolean opposingTeamEndMatchStatus) {
+        this.opposingTeamEndMatchStatus = opposingTeamEndMatchStatus;
     }
 
     @Builder
@@ -40,5 +52,7 @@ public class Apply extends TimeStamped {
         this.team = team;
         this.greeting = greeting;
         this.approved = approved;
+        this.endMatchStatus = false;
+        this.opposingTeamEndMatchStatus = false;
     }
 }
