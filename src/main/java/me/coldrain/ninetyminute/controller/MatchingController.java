c3c5b53d4258087a3045e2095b253d46ed8b00cc
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-07-14 20:43:58 +0900

    Fix: "수정 대결 신청 API, 대결 수락/ 거절 API"

--- src/main/java/me/coldrain/ninetyminute/controller/MatchingController.java
+++ src/main/java/me/coldrain/ninetyminute/controller/MatchingController.java
@@ -2,12 +2,9 @@ package me.coldrain.ninetyminute.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-<<<<<<< HEAD
 import me.coldrain.ninetyminute.dto.request.ApprovedMatchRequest;
 import me.coldrain.ninetyminute.dto.response.OfferMatchResponse;
-=======
 import me.coldrain.ninetyminute.dto.response.ApprovedMatchResponse;
->>>>>>> bb511c2 (Feat: "경기 성사 목록 조회 API")
 import me.coldrain.ninetyminute.security.UserDetailsImpl;
 import me.coldrain.ninetyminute.service.MatchingService;
 import org.springframework.http.HttpStatus;
@@ -26,7 +23,6 @@ public class MatchingController {
 
     /*
      * Author: 병민
-<<<<<<< HEAD
      * 대결 수락 목록 조회 API
      * 대결이 신청된 목록을 대결 신청 받은 팀의 팀장이 조회하는 API.
      */
@@ -39,20 +35,20 @@ public class MatchingController {
         return matchingService.searchOfferMatches(teamId, userDetails.getUser());
     }
 
-
     /*
      * Author: 병민
      * 대결 수락 정보 저장 API
      * 대결 수락 시 대결 상세 정보 저장 및 apply 상태 변경.
      */
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/teams/{apply_team_id}/apply")
-    public String searchApprovedMatch(
-            final @PathVariable Long apply_team_id,
+    @PostMapping("/teams/{apply_team_id}/apply/{apply_id}")
+    public String approveApplyMatch(
+            final @PathVariable("apply_team_id") Long applyTeamId,
+            final @PathVariable("apply_id") Long applyId,
             final @RequestBody ApprovedMatchRequest approvedMatchRequest,
             final @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        return matchingService.approvedMatch(apply_team_id, approvedMatchRequest, userDetails.getUser());
+        return matchingService.approveApplyMatch(applyTeamId, applyId, approvedMatchRequest, userDetails.getUser());
     }
 
     /*
@@ -61,23 +57,24 @@ public class MatchingController {
      * 대결 신청 팀의 id을 전달 받아야 함.
      */
     @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping("/teams/{apply_team_id}/apply")
+    @DeleteMapping("/teams/{apply_team_id}/apply/{apply_id}")
     public void cancelApplyMatch(
-            final @PathVariable Long apply_team_id,
+            final @PathVariable("apply_team_id") Long applyTeamId,
+            final @PathVariable("apply_id") Long applyId,
             final @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        matchingService.rejectApplyMatch(apply_team_id, userDetails.getUser().getOpenTeam().getId());
-=======
+        matchingService.rejectApplyMatch(applyTeamId, applyId, userDetails.getUser());
+    }
+
+    /*
      * 대결 성사 목록 조회 API
      * apply 의 approved 가 ture 일 때 목록을 조회 할 수 있습니다.
      */
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/teams/{team_id}/matches")
-    public List<ApprovedMatchResponse> searchApprovedMatch(
-            final @PathVariable Long team_id,
-            final @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
-        return matchingService.searchApprovedMatch(team_id, userDetails.getUser());
->>>>>>> bb511c2 (Feat: "경기 성사 목록 조회 API")
+    public List<ApprovedMatchResponse> searchApprovedMatch (
+    final @PathVariable("team_id") Long teamId,
+    final @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return matchingService.searchApprovedMatch(teamId, userDetails.getUser());
     }
 }
