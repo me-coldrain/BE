8cff277b5c2e740da2607972b0155e8df3bad60d
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-07-15 22:59:05 +0900

    Feat: "추가 대결 결과 등록 API "

---
+++ src/main/java/me/coldrain/ninetyminute/repository/SubstituteRepository.java
@@ -0,0 +1,18 @@
+package me.coldrain.ninetyminute.repository;
+
+import me.coldrain.ninetyminute.entity.FieldMember;
+import me.coldrain.ninetyminute.entity.SubstituteMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubstituteRepository extends JpaRepository<SubstituteMember, Long> {
+
+    @Query("select sm from SubstituteMember sm where sm.id =: fieldMemberId and sm.team.id =: teamId")
+    Optional<SubstituteMember> findByIdAndTeamId(Long substituteMemberId, Long teamId);
+
+    @Query("select sm from SubstituteMember sm where sm.team.id =: teamId and sm.afterMatching.id =: beforeMatchId and sm.anonymous = false ")
+    List<SubstituteMember> findMatchSubstituteMembers(Long teamId, Long afterMatchId);
+}
