f4e96b419b9ee76319f930332c0f1320a20d729f
guluming <guluming@naver.com>
2022-07-19 23:26:30 +0900

    Fix: "수정 로그인 사용자 개인 포지션 랭킹, 로그인 사용자가 참여하고 있는 팀의 랭킹 조회 API"
    - 팀,개인포지션 랭킹과 같은 순위 알고리즘을 적용
    
    - 로그인 사용자가 참여하고 있는 팀의 랭킹의 경우
          1순위 정렬 승점
          2순위 정렬 승률
          위 2개가 모두 같은 경우 동일 랭킹으로 표시
    
    -  로그인 사용자 개인 포지션 랭킹의 경우
          로그인 사용자의 포지션 포인트 값으로 정렬
          해당 값이 같으면 동일 랭킹으로 표시

--- src/main/java/me/coldrain/ninetyminute/dto/response/MyRankResponse.java
+++ src/main/java/me/coldrain/ninetyminute/dto/response/MyRankResponse.java
@@ -1,10 +1,10 @@
 package me.coldrain.ninetyminute.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class MyRankResponse {
--- src/main/java/me/coldrain/ninetyminute/repository/AbilityRepository.java
+++ src/main/java/me/coldrain/ninetyminute/repository/AbilityRepository.java
@@ -8,10 +8,6 @@ import java.util.List;
 public interface AbilityRepository extends JpaRepository<Ability, Long> {
 
     //개별 랭킹 확인을 위한 포지션별 랭킹 전체 조회
-    List<Ability> findAllByOrderByMvpPointDesc();
-
-    List<Ability> findAllByOrderByCharmingPointDesc();
-
     List<Ability> findAllByOrderByStrikerPointDesc();
 
     List<Ability> findAllByOrderByMidfielderPointDesc();
